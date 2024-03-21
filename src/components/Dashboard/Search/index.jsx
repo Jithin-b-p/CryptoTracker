1f29f87ec9deb5e75938cbb40e86dbd1aedadba8
unknown <jithinbp007@gmail.com>
2024-03-21 14:59:05 +0530

    adding a search component and styling it

---
+++ src/components/Dashboard/Search/index.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+
+import styles from "./styles.module.css";
+
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+
+function Search({ search, onSearchChange }) {
+  return (
+    <div className={styles.searchbar}>
+      <SearchRoundedIcon />
+      <input id="search" type="text" value={search} onChange={onSearchChange} />
+      <label className={`${search && styles.labeltranslate}`} htmlFor="search">
+        Enter a keyword...
+      </label>
+    </div>
+  );
+}
+
+export default Search;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,12 +2,23 @@ import { useEffect, useState } from "react";
 import Header from "../components/Common/Header";
 import TabsComponent from "../components/Dashboard/Tabs";
 import axios from "axios";
+import Search from "../components/Dashboard/Search";
 
 const API_URL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
 
 function DashboardPage() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   useEffect(() => {
     axios
       .get(API_URL)
@@ -20,7 +31,8 @@ function DashboardPage() {
   return (
     <>
       <Header />
-      <TabsComponent coins={coins} />
+      <Search search={search} onSearchChange={handleSearchChange} />
+      <TabsComponent coins={filteredCoins} />
     </>
   );
 }
